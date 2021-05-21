decda7befdc49bc60189e0ea46393fbff3ab875d
NaufalDzikrullah452 <naufal.dzikrullah452@gmail.com>
2021-05-21 23:28:57 +0700

    Connect to firebase and adding firebase auth

---
+++ app/src/main/java/com/example/reminderme/NotesDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.reminderme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotesDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_details);
+    }
+}
